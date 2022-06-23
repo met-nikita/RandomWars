f687ed8a2ddc274e89bd00c4e8477c2a3591f2bf
Никита Метельков <met-nikita@mail.ru>
2022-06-23 15:42:07 +0300

    add class User

---
+++ RandomWars/Data/User.cs
@@ -0,0 +1,9 @@
+﻿namespace RandomWars.Data
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
